73d98167a31aa205f9490c632d632e56aa251d22
nokoniko <94224292+nokoniko@users.noreply.github.com>
2025-10-10 13:00:14 +0200

    la til routs till alle nettsidene og lagde de nuvedige html filene

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def index() -> str:
     return render_template('index.html')
+
+@app.route('/meny')
+def meny() -> str:
+    return render_template('meny.html')
+
+@app.route('/kontakt')
+def kontakt() -> str:
+    return render_template('kontakt.html')
+
+@app.route('/varer')
+def varer() -> str:
+    return render_template('varer.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
