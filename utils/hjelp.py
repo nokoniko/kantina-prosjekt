947c2d385578810e7358481055e2c56ec6e3531a
nokoniko <94224292+nokoniko@users.noreply.github.com>
2025-10-15 16:44:34 +0200

    match statemes som sjekker hvilken dag det er som vill gi den rikitge siden

--- utils/hjelp.py
+++ utils/hjelp.py
@@ -1,14 +1,26 @@
+from flask import render_template
 import datetime
 
 class Hjelp:
     def __init__(self, name) -> None:
         self.name = name
 
-
     dag = datetime.datetime.now().strftime("%A")
 
+    def dagmeny(self) -> str:
+        match self.dag:
+            case "Monday":
+                return render_template('mandag.html')
+            case "Tusday":
+                return render_template('tirsdag.html')
+            case "Wednesday":
+                return render_template('onsdag.html')
+            case "Thursday":
+                return render_template('torsdag.html')
+            case "Friday":
+                return render_template('fredag.html')
 
 
-
-h = Hjelp(__name__)
-print(h.dag)
+if __name__ == "__main__":
+    h = Hjelp(__name__)
+    print(h.dag)
