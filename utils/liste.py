ca584ac200eb8a15d0450f0ec17883486c6ec9bb
nokoniko <94224292+nokoniko@users.noreply.github.com>
2025-10-17 08:42:26 +0200

    en liste med varene til kantinen

--- utils/liste.py
+++ utils/liste.py
@@ -6,3 +6,18 @@ menyen = [
     {"navn": "Ratatouille", "kategori": "Vegetarrett", "allergener": [], "bilde": "ratatouille.jpg", "dag": "Fredag"}
 ]
 
+varer = [
+    {"navn": "Sjokolademelk", "kategori": "Drikke", "allergener": ["melk"], "pris": 25},
+    {"navn": "Eplejuice", "kategori": "Drikke", "allergener": [], "pris": 20},
+    {"navn": "Mineralvann", "kategori": "Drikke", "allergener": [], "pris": 22},
+    {"navn": "Nøtteblanding", "kategori": "Snack", "allergener": ["nøtter"], "pris": 30},
+    {"navn": "Yoghurt med granola", "kategori": "Snack", "allergener": ["melk", "gluten", "nøtter"], "pris": 35},
+    {"navn": "Croissant", "kategori": "Bakverk", "allergener": ["gluten", "melk"], "pris": 28},
+    {"navn": "Baguette med ost og skinke", "kategori": "Mat", "allergener": ["gluten", "melk"], "pris": 55},
+    {"navn": "Salatboks", "kategori": "Mat", "allergener": ["egg"], "pris": 60},
+    {"navn": "Fruktbeger", "kategori": "Snack", "allergener": [], "pris": 30},
+    {"navn": "Kaffe", "kategori": "Drikke", "allergener": [], "pris": 20},
+    {"navn": "Te", "kategori": "Drikke", "allergener": [], "pris": 18},
+    {"navn": "Brownie", "kategori": "Bakverk", "allergener": ["gluten", "melk", "egg"], "pris": 32}
+]
+
