84188889227b0936aca6e005af4b94e86593f1b6
nokoniko <94224292+nokoniko@users.noreply.github.com>
2025-10-20 10:07:01 +0200

    Oppdatert varer med bilder og priser, samt endret navn og stilling på kontaktpersoner. La til oversettelse for ukedager.

--- utils/liste.py
+++ utils/liste.py
@@ -12,27 +12,33 @@ class Liste:
     ]
 
     varere = [
-        {"navn": "Sjokolademelk", "kategori": "Drikke", "allergener": ["melk"], "pris": 25},
-        {"navn": "Eplejuice", "kategori": "Drikke", "allergener": [], "pris": 20},
-        {"navn": "Mineralvann", "kategori": "Drikke", "allergener": [], "pris": 22},
-        {"navn": "Nøtteblanding", "kategori": "Snack", "allergener": ["nøtter"], "pris": 30},
-        {"navn": "Yoghurt med granola", "kategori": "Snack", "allergener": ["melk", "gluten", "nøtter"], "pris": 35},
-        {"navn": "Croissant", "kategori": "Bakverk", "allergener": ["gluten", "melk"], "pris": 28},
-        {"navn": "Baguette med ost og skinke", "kategori": "Mat", "allergener": ["gluten", "melk"], "pris": 55},
-        {"navn": "Salatboks", "kategori": "Mat", "allergener": ["egg"], "pris": 60},
-        {"navn": "Fruktbeger", "kategori": "Snack", "allergener": [], "pris": 30},
-        {"navn": "Kaffe", "kategori": "Drikke", "allergener": [], "pris": 20},
-        {"navn": "Te", "kategori": "Drikke", "allergener": [], "pris": 18},
-        {"navn": "Brownie", "kategori": "Bakverk", "allergener": ["gluten", "melk", "egg"], "pris": 32}
+        {"navn": "Sjokolademelk", "kategori": "Drikke", "allergener": ["melk"], "bilde": "sjokolademelk.jpeg", "pris": 68},
+        {"navn": "Eplejuice", "kategori": "Drikke", "allergener": [], "bilde": "epplejuice.jpg", "pris": 62},
+        {"navn": "Mineralvann", "kategori": "Drikke", "allergener": [], "bilde": "vann.jpg", "pris": 52},
+        {"navn": "Nøtteblanding", "kategori": "Snack", "allergener": ["nøtter"], "bilde": "notter.jpg", "pris": 42},
+        {"navn": "Yoghurt med granola", "kategori": "Snack", "allergener": ["melk", "gluten", "nøtter"], "bilde": "yoghurt.jpeg", "pris": 58},
+        {"navn": "Croissant", "kategori": "Bakverk", "allergener": ["gluten", "melk"], "bilde": "Croissant.jpg", "pris": 50},
+        {"navn": "Baguette med ost og skinke", "kategori": "Mat", "allergener": ["gluten", "melk"], "bilde": "bagguete.jpg", "pris": 100},
+        {"navn": "Salatboks", "kategori": "Mat", "allergener": ["egg"], "bilde": "salat.Jpeg", "pris":120},
+        {"navn": "Fruktbeger", "kategori": "Snack", "allergener": [], "bilde": "fruktBeger.jpg", "pris": 90},
+        {"navn": "Kaffe", "kategori": "Drikke", "allergener": [], "bilde": "kaffe.jpg", "pris": 33},
+        {"navn": "Te", "kategori": "Drikke", "allergener": [], "bilde": "te.png", "pris": 33},
+        {"navn": "Brownie", "kategori": "Bakverk", "allergener": ["gluten", "melk", "egg"], "bilde": "brownie.webp", "pris": 67}
     ]
 
     kontanktpersoner = [
-        {"navn": "Nikolai", "stilling": "Sigma-sjef", "bilde": "nikolai.jpeg", "epost": "nikolai.kantine@yessirski.com", "tlf": "400 12 345"},
-        {"navn": "Audun", "stilling": "Vaskehjelp", "bilde": "audun.jpeg", "epost": "audun.kantine@yessirski.com", "tlf": "401 23 456"},
-        {"navn": "Ebbe", "stilling": "Lokal bidragsyter", "bilde": "ebbe.jpeg", "epost": "ebbe.kantine@yessirski.com", "tlf": "402 34 567"},
-        {"navn": "Helene", "stilling": "Kasse", "bilde": "helene.jpeg", "epost": "helene.kantine@yessirski.com", "tlf": "403 45 678"},
-        {"navn": "Ludvig", "stilling": "Assistent", "bilde": "ludvig.jpeg", "epost": "ludvig.kantine@yessirski.com", "tlf": "404 56 789"},
-        {"navn": "Sebastian", "stilling": "kokk", "bilde": "sebastian.jpeg", "epost": "sebastian.kantine@yessirski.com", "tlf": "405 67 890"}
+        {"navn": "Nikolai pai", "stilling": "Sigma-sjef", "bilde": "nikolai.jpeg", "epost": "nikolai.kantine@yessirski.com", "tlf": "400 12 345"},
+        {"navn": "Audun bao bum", "stilling": "Vaskehjelp", "bilde": "audun.jpeg", "epost": "audun.kantine@yessirski.com", "tlf": "401 23 456"},
+        {"navn": "Ebbe Lebbe", "stilling": "Lokal narkoman", "bilde": "ebbe.jpg", "epost": "ebbe.kantine@yessirski.com", "tlf": "402 34 567"},
+        {"navn": "Helene grene", "stilling": "Kasen", "bilde": "helene.jpeg", "epost": "helene.kantine@yessirski.com", "tlf": "403 45 678"},
+        {"navn": "Ludvig sudvig", "stilling": "Assistent", "bilde": "ludvig.jpeg", "epost": "ludvig.kantine@yessirski.com", "tlf": "404 56 789"},
+        {"navn": "Sebastian nastian", "stilling": "kokk", "bilde": "sebastian.jpeg", "epost": "sebastian.kantine@yessirski.com", "tlf": "405 67 890"}
     ]
-
-
+    
+    oversettelse = {
+        "Monday": "Mandag",
+        "Tuesday": "Tirsdag",
+        "Wednesday": "Onsdag",
+        "Thursday": "Torsdag",
+        "Friday": "Fredag"
+    }
